4df72331b7b1b25cad98a5dabc42b08be1454033
Cesar Hoffmann <ext.choffmann@riotgames.com>
2025-02-06 02:06:21 -0300

    remove unused effect between card person name and suit

--- app/components/ui/card-details/cardSuit.tsx
+++ app/components/ui/card-details/cardSuit.tsx
@@ -10,8 +10,7 @@ const CardSuit = ({
 
   return (
     <div className={`flex items-center justify-center gap-2 ${textColor}`}>
-      {/* <span className="inline-block w-2 h-2 mx-2 bg-gray-800 rounded-full"></span> */}
-      <span className="text-5xl mb-1">{suit}</span>
+      <span className="text-4xl mb-1">{suit}</span>
       <span className="text-4xl font-thin font-serif">{value}</span>
     </div>
   );
