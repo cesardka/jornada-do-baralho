a3e765838523b942cd343a50efc60f63e63b7353
Cesar Hoffmann <ext.choffmann@riotgames.com>
2025-07-06 07:33:32 -0300

    update footer to include my name and link to my ig

--- app/components/ui/footer/index.tsx
+++ app/components/ui/footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useAnimation } from "../../AnimationContext";
 
 export default function Footer() {
@@ -7,8 +8,17 @@ export default function Footer() {
   if (!animationEnded) return;
 
   return (
-    <div className="w-full h-20 p-10 flex items-center justify-center text-white text-xl bg-blue-600 z-10">
+    <div className="w-full h-14 p-5 flex items-center justify-center text-white text-sm bg-blue-600 z-10">
+      <Link
+        href="https://www.instagram.com/cesardka"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mr-1 font-bold"
+      >
+        César Hoffmann
+      </Link>{" "}
       © {copyrightYear}
+      {/* I don't think this copyright symbol means much unless I go after do the paperwork, right? Hopefully not -- */}
     </div>
   );
 }
