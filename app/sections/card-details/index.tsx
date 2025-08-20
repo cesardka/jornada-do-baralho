d9b1045c028cdb9b55f1ea66cbbc26e89649240d
Cesar Hoffmann <cesardka@hotmail.com>
2025-08-20 03:03:04 -0300

    fix pending sign and social media images

--- app/sections/card-details/index.tsx
+++ app/sections/card-details/index.tsx
@@ -138,7 +138,7 @@ const CardDetails = ({
               <Image
                 width={960}
                 height={540}
-                src="/images/ilustra-sem-redes.png"
+                src="/images/illustrations/ilustra-sem-redes.png"
                 alt="Não encontramos as redes"
                 className="w-full h-auto transition duration-300 ease-in-out group-hover:blur-md"
               />
@@ -189,7 +189,7 @@ const CardDetails = ({
               <Image
                 width={960}
                 height={540}
-                src="/images/ilustra-sem-assinatura.png"
+                src="/images/illustrations/ilustra-sem-assinatura.png"
                 alt="Carta pendente assinatura"
                 className="w-full h-auto transition duration-300 ease-in-out group-hover:blur-md"
               />
